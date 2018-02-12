b8c530f638d3cfd5e484a31468e88bba85d5316d
KelvinRod9331 <kelvinrodriguez@ac.c4q.nyc>
2018-02-12 15:25:27 -0500

    I fixed some errors and Clean/Organized Some Merged Code

--- back-end/db/queries.js
+++ back-end/db/queries.js
@@ -1,22 +1,24 @@
 
-const db = require('./index.js');
+const db = require("./index");
+const authHelpers = require("../auth/helpers");
+const passport = require("../auth/local");
 
 const getAllPhotos = (req, res, next) => {
   db
-  .any('select * from photos')
-  .then(data => {
-    res.status(200).json({
-      status: 'success',
-      data: data,
-      message: 'Retrieved all photos'
-    });
-  })
-  .catch(err => next(err))
+    .any('select * from photos')
+    .then(data => {
+      res.status(200).json({
+        status: 'success',
+        data: data,
+        message: 'Retrieved all photos'
+      });
+    })
+    .catch(err => next(err))
 }
 
 const getUserPhotos = (req, res, next) => {
   db
-    .any('select * from photos where user_ID = ${id}', {id: user.id})
+    .any('select * from photos where user_ID = ${id}', { id: user.id })
     .then(data => {
       res.status(200).json({
         status: 'success',
@@ -29,7 +31,7 @@ const getUserPhotos = (req, res, next) => {
 
 const getUserFollowers = (req, res, next) => {
   db
-    .any('select * from follows where follower_ID = ${id}', {id: user.id})
+    .any('select * from follows where follower_ID = ${id}', { id: user.id })
     .then(data => {
       res.status(200).json({
         status: 'success',
@@ -42,7 +44,7 @@ const getUserFollowers = (req, res, next) => {
 
 const getUserFollowing = (req, res, next) => {
   db
-    .any('select * from follows where user_ID = ${id}', {id: user.id})
+    .any('select * from follows where user_ID = ${id}', { id: user.id })
     .then(data => {
       res.status(200).json({
         status: 'success',
@@ -55,7 +57,7 @@ const getUserFollowing = (req, res, next) => {
 
 const getPhotoLikes = (req, res, next) => {
   db
-    .any('select * from likes where photo_ID = ${photoid}', {photoid: photo.id})
+    .any('select * from likes where photo_ID = ${photoid}', { photoid: photo.id })
     .then(data => {
       res.status(200).json({
         status: 'success',
@@ -66,7 +68,7 @@ const getPhotoLikes = (req, res, next) => {
     .catch(err => next(err))
 }
 
-const uploadPhoto = (req, res next) => {
+const uploadPhoto = (req, res, next) => {
   db
     .none('insert into photos (user_ID, url) values (${id}, ${url})', req.body)
     .then(() => {
@@ -82,6 +84,67 @@ const likePhoto = (req, res, next) => {
     none('insert into likes (user_ID, photo_ID) values (${userid}, ${photoid})', req.body)
 }
 
+function getAllUsers(req, res, next) {
+  db
+    .any("select * from users")
+    .then(function (data) {
+      res.status(200).json({
+        status: "success",
+        data: data,
+        message: "Retrieved ALL users"
+      });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+}
+
+function loginUser(req, res, next) {
+
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      res.status(500).send("error while trying to log in");
+    } else if (!user) {
+      res.status(401).send("invalid username/password");
+    } else if (user) {
+      req.logIn(user, function (err) {
+        if (err) {
+          res.status(500).send("error");
+        } else {
+          res.status(200).send(user);
+        }
+      });
+    }
+  })(req, res, next);
+}
+
+function logoutUser(req, res, next) {
+  req.logout();
+  res.status(200).send("log out success");
+}
+
+function registerUser(req, res, next) {
+  return authHelpers
+    .createUser(req)
+    .then(response => {
+      passport.authenticate("local", (err, user, info) => {
+        if (user) {
+          res.status(200).json({
+            status: "success",
+            data: user,
+            message: "Registered one user"
+          });
+        }
+      })(req, res, next);
+    })
+    .catch(err => {
+      res.status(500).json({
+        status: "error",
+        error: err
+      });
+    });
+}
+
 module.exports = {
   getAllPhotos,
   getUserPhotos,
@@ -89,78 +152,10 @@ module.exports = {
   getUserFollowing,
   getPhotoLikes,
   uploadPhoto,
-  likePhoto
-}
-=======
-const db = require("./index");
-const authHelpers = require("../auth/helpers");
-const passport = require("../auth/local");
-
-function getAllUsers(req, res, next) {
-  db
-    .any("select * from users")
-    .then(function(data) {
-      res.status(200).json({
-        status: "success",
-        data: data,
-        message: "Retrieved ALL users"
-      });
-    })
-    .catch(function(err) {
-      return next(err);
-    });
-}
-
-function loginUser(req, res, next) {
-    
-    passport.authenticate("local", (err, user, info) => {
-      if (err) {
-        res.status(500).send("error while trying to log in");
-      } else if (!user) {
-        res.status(401).send("invalid username/password");
-      } else if (user) {
-        req.logIn(user, function(err) {
-          if (err) {
-            res.status(500).send("error");
-          } else {
-            res.status(200).send(user);
-          }
-        });
-      }
-    })(req, res, next);
-}
-  
-function logoutUser(req, res, next) {
-    req.logout();
-    res.status(200).send("log out success");
-}
-
-function registerUser(req, res, next) {
-    return authHelpers
-      .createUser(req)
-      .then(response => {
-        passport.authenticate("local", (err, user, info) => {
-          if (user) {
-            res.status(200).json({
-              status: "success",
-              data: user,
-              message: "Registered one user"
-            });
-          }
-        })(req, res, next);
-      })
-      .catch(err => {
-        res.status(500).json({
-          status: "error",
-          error: err
-        });
-      });
-}
-
-module.exports = {
-    getAllUsers: getAllUsers,
-    registerUser: registerUser,
-    loginUser: loginUser,
-    logoutUser: logoutUser
-  };
+  likePhoto,
+  getAllUsers,
+  registerUser,
+  loginUser,
+  logoutUser,
+};
 
