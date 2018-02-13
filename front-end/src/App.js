3389f2c87e88e752205b75fddf510a2495f15185
janhairis <janhairisfermin@ac.c4q.nyc>
2018-02-13 14:50:45 -0500

    first commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,10 +6,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <div class='main'></div>
+        <nav id='header'>
+          <div class='header'>
+            <div className="App-title">Instagram</div>
+            <div class='icon'>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+            <i class="fa fa-instagram" ></i>
+            </div>
+          </div>
+        </nav>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
