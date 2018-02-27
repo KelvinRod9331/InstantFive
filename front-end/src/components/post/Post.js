6dbfee36875aa6310f1177268b8ea6da8c61cea5
KelvinRod9331 <kelvinrodriguez@ac.c4q.nyc>
2018-02-27 17:38:54 -0500

    Added Post Table and Routes

---
+++ front-end/src/components/post/Post.js
@@ -0,0 +1,19 @@
+// import React, {Component} from 'react'
+// import axios from 'axios'
+
+
+
+
+// class Post extends Component {
+//     constructor(){
+//         super()
+//         this.state({
+//             comments: [],
+//             likes: []
+//         })
+//     }
+
+//     retrievePost
+
+
+// }
